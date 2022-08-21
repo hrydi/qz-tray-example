310d6bf2017ad5768d32e0d7738b5a9a68ba5bf2
wearehardinatas <admin@wearehardinata.com>
2022-08-21 23:40:25 +0700

    adding cors fpr testing

--- www/cert.php
+++ www/cert.php
@@ -1,4 +1,6 @@
 <?php
+require_once './lib.php';
+cors();
 header('Content-Type: plain/text');
 echo file_get_contents('/var/www/certs/cert.pem');
 exit(0);
--- www/server.php
+++ www/server.php
@@ -1,11 +1,14 @@
 <?php
+require_once './lib.php';
+cors();
+$passphrase = getenv('SSL_PASS_KEY');
 $key = '/var/www/certs/key.pem';
 $req = $_GET['request'];
-$privateKey = openssl_get_privatekey(file_get_contents($key), 'your-passphrase');
+$privateKey = openssl_get_privatekey(file_get_contents($key), $passphrase);
 $signature = NULL;
-openssl_sign($req, $signature, $privateKey, OPENSSL_ALGO_SHA512);
+openssl_sign($req, $signature, $privateKey, 'sha512');
 
-if($signature) {
+if ($signature) {
   header("Content-Type: text/plain");
   echo base64_encode($signature);
   exit(0);
